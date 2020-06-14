b04d3ade9cdebe8cc08c71a92ad83d6de0fa0d0a
Ignat Insarov <kindaro@gmail.com>
2020-06-15 01:43:44 +0300

    Use right fold. Victory!
        λ ("x"…"y"…"z") 1 2
        "x1y2z"

--- X.hs
+++ X.hs
@@ -6,16 +6,23 @@ import Data.String
 
 class Function a where
   applyAfter :: a -> (String -> String) -> a
+  applyBefore :: (String -> String) -> a -> a
 
 instance Function String where
   applyAfter f g = g f
+  applyBefore g f = g f
 
 instance (Function a, Show b) => Function (b -> a) where
   applyAfter f g = fmap (`applyAfter` g) f
+  applyBefore g f = fmap (g `applyBefore`) f
 
 
-(…) :: (Function a, Show s) => a -> String -> (s -> a)
-(…) f x = \u -> f × (\s -> s ++ show u ++ x)
+infixr 1 …
+(…) :: (Function a, Show s) => String -> a -> (s -> a)
+(…) x f = \u -> (\s -> x ++ show u ++ s) ÷ f
 
 (×) :: Function a => a -> (String -> String) -> a
 (×) = applyAfter
+
+(÷) ∷ Function a ⇒ (String → String) → a → a
+(÷) = applyBefore
