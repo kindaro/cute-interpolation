2b2da64389693e446d34261e2e62e6996e0a7c85
Ignat Insarov <kindaro@gmail.com>
2020-06-15 03:23:33 +0300

    Use class magic to ensure composability.
        λ let a = ("u" ÷ show ÷ "x" ÷ show)
        λ let b = "y"
        λ let c = (show ÷ "z")
        λ (a ÷ b ÷ c) 1 2 3
        "u1x2y3z"

--- X.hs
+++ X.hs
@@ -1,28 +1,18 @@
-{-# language UnicodeSyntax #-}
+{-# language UnicodeSyntax, MultiParamTypeClasses, FunctionalDependencies, UndecidableInstances, TypeFamilies #-}
 
 module X where
 
 import Data.String
 
-class Function a where
-  applyAfter :: a -> (String -> String) -> a
-  applyBefore :: (String -> String) -> a -> a
+class Function a b c | a b → c, a c → b where
+  (÷) ∷ a → b → c
+  infixl 1 ÷
 
-instance Function String where
-  applyAfter f g = g f
-  applyBefore g f = g f
+instance Function String String String where
+   f ÷ g = f <> g
 
-instance (Function a, Show b) => Function (b -> a) where
-  applyAfter f g = fmap (`applyAfter` g) f
-  applyBefore g f = fmap (g `applyBefore`) f
+instance Function a b c ⇒ Function (m → a) b (m → c) where
+  f ÷ g = \x → f x ÷ g
 
-
-infixr 1 …
-(…) :: (Function a, Show s) => String -> a -> (s -> a)
-(…) x f = \u -> (\s -> x ++ show u ++ s) ÷ f
-
-(×) :: Function a => a -> (String -> String) -> a
-(×) = applyAfter
-
-(÷) ∷ Function a ⇒ (String → String) → a → a
-(÷) = applyBefore
+instance {-# overlappable #-} Function String b b ⇒ Function String (m → b) (m → b) where
+  f ÷ g = \x → f ÷ g x
