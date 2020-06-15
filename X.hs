8f24218b7a349e4bc0cf5733b3e36fbcd9eefc01
Ignat Insarov <kindaro@gmail.com>
2020-06-15 05:49:42 +0300

    Total victory.
        λ let a = ("u" … "v" … "w")
        λ let b = "/"
        λ let c = ("x" … "y" … "z")
        λ putStrLn $ (a … b … c) 1 2 3 4 5 6
        u1v2w3/4x5y6z

--- X.hs
+++ X.hs
@@ -17,20 +17,10 @@ instance Group a b c ⇒ Group (m → a) b (m → c) where
 instance {-# overlappable #-} Group String b b ⇒ Group String (m → b) (m → b) where
   f ÷ g = \x → f ÷ g x
 
-data Function k = Terminal k | Argument k (Function k)
+type family (f ∷ *) <> (g ∷ *) where
+  (a → f) <> g = a → (f <> g)
+  a <> (b → g) = b → (a <> g)
+  a <> a = a
 
-type family Decode (f ∷ Function k) = (r ∷ *) where
-  Decode (Terminal t) = t
-  Decode (Argument t ts) = t → Decode ts
-
-type family Encode (f ∷ *) = (r ∷ Function *) | r →f  where
-  Encode (a → b) = Argument a (Encode b)
-  Encode t = Terminal t
-
-type family (f ∷ Function k) <> (g ∷ Function k) where
-  Terminal t <> Terminal t = Terminal t
-  Terminal t <> Argument a g = Argument a (Terminal t <> g)
-  Argument a f <> g = Argument a (f <> g)
-
-(…) ∷ f → g → Decode (Encode f <> Encode (s → String) <> Encode g)
-f … g = _u
+(…) ∷ _ ⇒ f → g → f <> (s → String) <> g
+f … g = f ÷ show ÷ g
